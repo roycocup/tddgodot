37c6e2df43e4f99d4c0cf6e986954ab235f0846c
Rodrigo Dias <rodrigo.dias@browsergroup.com>
2019-02-16 18:26:37 +0000

    gitting the snake to move

--- scripts/Snake.gd
+++ scripts/Snake.gd
@@ -1,7 +1,36 @@
 extends Node2D
 
+enum {DIR_UP, DIR_DOWN, DIR_LEFT, DIR_RIGHT}
+
+var speed = 10
+var direction = get_random_dir()
+
 func _ready():
 	pass
 
 func _process(delta):
-	position.x = position.x + 1
+	move(delta)
+
+func move(delta):
+	if direction == DIR_UP:
+		position.y = position.y - (delta * speed)
+	if direction == DIR_DOWN:
+		position.y = position.y + (delta * speed)
+	if direction == DIR_LEFT:
+		position.x = position.x - (delta * speed)
+	if direction == DIR_RIGHT:
+		position.x = position.x + (delta * speed)	
+
+func get_random_dir():
+	var i = randi() % 3
+	if i == 0:
+		return DIR_UP
+	if i == 1:
+		return DIR_DOWN
+	if i == 2:
+		return DIR_LEFT
+	if i == 3:
+		return DIR_RIGHT
+
+func set_direction(dir):
+	direction = dir
--- test/unit/test_snake.gd
+++ test/unit/test_snake.gd
@@ -1,16 +1,42 @@
 extends "res://addons/gut/test.gd"
 
+var snake
+
+func before_all():
+	snake = load("res://scenes/Snake.tscn").instance()
 
 func after_all():
-	get_tree().queue_free()
+	snake.queue_free()
 
-func test_snake_moves():
-	var snake = load("res://scenes/Snake.tscn").instance()
-	#var snake = Snake.instance()
+func test_snake_moves():	
 	var initial = snake.position
 	gut.simulate(snake, 5, .1)
 	var later = snake.position
 	assert_ne(initial, later, "position for snake is the same")
+
+func test_random_direction_returns_one_of_4():
+	var result = snake.get_random_dir()
+	assert(result == snake.DIR_UP || result == snake.DIR_DOWN || result == snake.DIR_RIGHT || result == snake.DIR_LEFT)
+
+func test_if_direction_is_set_snake_moves_in_that_direction():
+	snake.set_direction(snake.DIR_UP)
+	var start = snake.position
+	gut.simulate(snake, 1, .1)
+	var end = snake.position
+	assert_ne(start, end, "snake is not moving")
+	assert_lt(end[1], start[1], "not moving up")
+
+
+func test_snake_is_moving_in_correct_speed():
+	snake.set_direction(snake.DIR_UP)
+	var start = snake.position
+	gut.simulate(snake, 1, .1)
+	var end = snake.position
+	
+	# expect the y to move this much
+	var expected = start[1]-( snake.speed * .1)
+	assert_eq(end[1], expected, "snake speed is not correct")
+
 
 	
 

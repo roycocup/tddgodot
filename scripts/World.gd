6413a3c158e70942da1f27856f887ebaa07054a2
Rodrigo Dias <rodrigo.dias@browsergroup.com>
2019-02-17 08:15:17 +0000

    programatic ways to create scenes and nodes

--- scripts/World.gd
+++ scripts/World.gd
@@ -2,7 +2,9 @@ extends Node2D
 
 
 func _ready():
-	create_scene("Grid2", Node2D.new(), "Grid")
+	create_grid()
+	create_fruit()
+	create_snake()
 	set_process(true)
 
 func _process(delta):
@@ -12,6 +14,16 @@ func check_quit():
 	if (Input.is_key_pressed(KEY_ESCAPE)):
 		get_tree().quit()
 
+
+
+func create_fruit():
+	create_scene("Fruit", Node2D.new(), "Fruit")
+
+func create_snake():
+	create_scene("Snake", Node2D.new(), "Snake")
+
+func create_grid():
+	create_scene("Grid", Node2D.new(), "Grid")
 
 func create_scene(name, root_node, script_name = null):
 	var grid = PackedScene.new()
